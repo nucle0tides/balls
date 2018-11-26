74caf7c95f551a774b6ae1843b59e45d92648f2b
Gabby Ortman <ortmangl@gmail.com>
2018-11-25 23:29:00 -0600

    drawing lots o balls

--- bouncing-balls/src/lib.rs
+++ bouncing-balls/src/lib.rs
@@ -1,33 +1,36 @@
 extern crate js_sys;
 extern crate wasm_bindgen;
 extern crate web_sys;
-extern crate rand;
+extern crate wbg_rand;
 
+use wbg_rand::{Rng, wasm_rng};
 use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use rand::thread_rng;
-use rand::seq::SliceRandom;
 
 struct Ball {
-    x_speed: i32,
-    y_speed: i32,
-    radius: i32,
+    x_speed: f64,
+    y_speed: f64,
+    radius: f64,
     ratio: i32,
-    x_origin: i32,
-    y_origin: i32,
+    x: f64,
+    y: f64,
+    x_acceleration: f64,
+    y_acceleration: f64,
     color: String,
 }
 
 impl Ball {
     fn new(color: String) -> Ball {
         Ball {
-            x_speed: 5,
-            y_speed: 2,
-            radius: 10,
+            x_speed: 5.0,
+            y_speed: 2.0,
+            radius: 10.0,
             ratio: 1,
-            x_origin: 0,
-            y_origin: 0,
+            x: 15.0,
+            y: 15.0,
+            x_acceleration: 1.0,
+            y_acceleration: 1.0,
             color,
         }
     }
@@ -40,25 +43,14 @@ impl Ball {
 //     return color
 // }
 
-fn setup() {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
-
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-}
+#[wasm_bindgen]
+pub struct ClosureHandle(Closure<FnMut()>);
 
 #[wasm_bindgen]
 pub fn draw() {
+    let window = web_sys::window().unwrap();
+    let window_width = window.inner_width().unwrap().as_f64().unwrap();
+    let window_height = window.inner_height().unwrap().as_f64().unwrap();
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas
@@ -74,7 +66,48 @@ pub fn draw() {
         .unwrap();
 
     let mut balls: Vec<Ball> = Vec::new(); 
-    for n in 0..50 {
-        balls.push(Ball::new(String::from("#526055")));
+    for _n in 0..50 {
+        balls.push(Ball::new(String::from("#5a5ca7")));
     }
+
+    let rand_num = || wasm_rng().gen_range(0.0, window_height);;
+    let draw = || {
+        // context.clear_rect(0.0, 0.0, window_width, window_height);
+        for mut b in balls {
+            b.x = wasm_rng().gen_range(0.0, window_width);
+            b.y = wasm_rng().gen_range(0.0, window_height);
+            context.set_fill_style(&JsValue::from(b.color));
+            context.begin_path();
+            context.arc(b.x, b.y, b.radius, 0.0, f64::consts::PI * 2.0).unwrap();
+            context.fill();
+        }
+    };
+
+    draw();
+
+    // let _move_balls = || {
+    //     for mut b in balls {
+    //         b.x_acceleration += b.x_speed * rand::random::<f64>();
+    //         b.y_acceleration += b.y_speed * rand::random::<f64>();
+    //         b.x += b.x_acceleration;
+    //         b.y += b.y_acceleration;
+
+    //         if b.x > window_width - b.radius || b.x < b.radius {
+    //             b.x_acceleration = b.x_acceleration * -1.0;
+    //         }
+
+    //         if b.y > window_height - b.radius || b.y < b.radius {
+    //             b.y_acceleration = b.y_acceleration * -1.0;
+    //         }
+    //     }
+    // };
+
+
+    // let animate = Closure::wrap(Box::new(move || {
+
+    // }) as Box<FnMut()>);
+
+    // window.request_animation_frame(animate.as_ref().unchecked_ref());
+
+    // ClosureHandle(animate);
 }
--- bouncing-balls/www/index.js
+++ bouncing-balls/www/index.js
@@ -1,3 +1,10 @@
 import * as wasm from "bouncing-balls";
 
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+let canvas = document.getElementById('canvas');
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
 wasm.draw();
