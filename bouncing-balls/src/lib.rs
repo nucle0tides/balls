50aaadb3066c0f6026bb1709d43efc84589741f2
Gabby Ortman <ortmangl@gmail.com>
2019-01-20 09:36:31 -0600

    compiles, RAF loop runs, balls aren't...moving?

--- bouncing-balls/src/lib.rs
+++ bouncing-balls/src/lib.rs
@@ -3,17 +3,19 @@ extern crate wasm_bindgen;
 extern crate web_sys;
 extern crate wbg_rand;
 
+use std::cell::RefCell;
+use std::rc::Rc;
 use wbg_rand::{Rng, wasm_rng};
 use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use web_sys::console;
 
 #[derive(Copy, Clone)]
 struct Ball {
     x_speed: f64,
     y_speed: f64,
     radius: f64,
-    ratio: i32,
     x: f64,
     y: f64,
     x_acceleration: f64,
@@ -27,12 +29,31 @@ impl Ball {
             x_speed: 5.0,
             y_speed: 2.0,
             radius: 10.0,
-            ratio: 1,
-            x: 15.0,
-            y: 15.0,
+            x: 1.0,
+            y: 1.0,
             x_acceleration: 1.0,
             y_acceleration: 1.0,
             color,
+        }
+    }
+
+    fn set_ball(mut self) {
+        self.x = 30.0;
+        self.y = 450.0;
+    }
+
+    fn move_ball(mut self) {
+        self.x_acceleration += self.x_speed * wasm_rng().gen_range(0.0, window_width());
+        self.y_acceleration += self.y_speed * wasm_rng().gen_range(0.0, window_height());
+        self.x += self.x_acceleration;
+        self.y += self.y_acceleration;
+
+        if self.x > window_width() - self.radius || self.x < self.radius {
+            self.x_acceleration = self.x_acceleration * -1.0;
+        }
+
+        if self.y > window_height() - self.radius || self.y < self.radius {
+            self.y_acceleration = self.y_acceleration * -1.0;
         }
     }
 }
@@ -44,16 +65,33 @@ impl Ball {
 //     return color;
 // }
 
-#[wasm_bindgen]
-pub struct ClosureHandle(Closure<FnMut()>);
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+fn document() -> web_sys::Document {
+    window()
+        .document()
+        .expect("should have a document on window")
+}
+
+fn window_width() -> f64 {
+    window().inner_width().unwrap().as_f64().unwrap()
+}
+
+fn window_height() -> f64 {
+    window().inner_height().unwrap().as_f64().unwrap()
+}
 
 #[wasm_bindgen]
 pub fn draw() {
-    let window = web_sys::window().unwrap();
-    let window_width = window.inner_width().unwrap().as_f64().unwrap();
-    let window_height = window.inner_height().unwrap().as_f64().unwrap();
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas = document().get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
@@ -71,44 +109,35 @@ pub fn draw() {
         balls.push(Ball::new(0x5B7373));
     }
 
-    let rand_num = || wasm_rng().gen_range(0.0, window_height);;
-    let draw = || {
-        context.clear_rect(0.0, 0.0, window_width, window_height);
-        for b in balls {
-            // b.x = wasm_rng().gen_range(0.0, window_width);
-            // b.y = wasm_rng().gen_range(0.0, window_height);
-            context.set_fill_style(&JsValue::from(b.color));
+    context.clear_rect(0.0, 0.0, window_width(), window_height());
+    for b in &mut balls {
+        b.set_ball();
+    }
+
+    for b in &mut balls {
+        context.set_fill_style(&JsValue::from("#BADA55"));
+        context.begin_path();
+        context.arc(b.x, b.y, b.radius, 0.0, f64::consts::PI * 2.0).unwrap();
+        context.fill();
+    }
+
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
+
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        console::log_1(&"RAF Loop".into());
+        context.clear_rect(0.0, 0.0, window_width(), window_height());
+        for b in &mut balls {
+            context.set_fill_style(&JsValue::from("#BADA55"));
             context.begin_path();
             context.arc(b.x, b.y, b.radius, 0.0, f64::consts::PI * 2.0).unwrap();
             context.fill();
         }
-    };
-
-    draw();
-
-    let _move_balls = || {
-        for mut b in balls {
-            b.x_acceleration += b.x_speed * wasm_rng().gen_range(0.0, window_width);
-            b.y_acceleration += b.y_speed * wasm_rng().gen_range(0.0, window_height);
-            b.x += b.x_acceleration;
-            b.y += b.y_acceleration;
-
-            if b.x > window_width - b.radius || b.x < b.radius {
-                b.x_acceleration = b.x_acceleration * -1.0;
-            }
-
-            if b.y > window_height - b.radius || b.y < b.radius {
-                b.y_acceleration = b.y_acceleration * -1.0;
-            }
+        for mut b in &mut balls {
+            b.move_ball();
         }
-    };
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<FnMut()>));
 
-
-    // let animate = Closure::wrap(Box::new(move || {
-
-    // }) as Box<FnMut()>);
-
-    // window.request_animation_frame(animate.as_ref().unchecked_ref());
-
-    // ClosureHandle(animate);
+    request_animation_frame(g.borrow().as_ref().unwrap());
 }
